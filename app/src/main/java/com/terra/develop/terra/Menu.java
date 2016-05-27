d652936c8d96b783a948afd844175589e4b5c7cb
Vincenzo <vincenzo.copia@gmail.com>
2016-05-27 23:18:47 +0200

    test2002

--- app/src/main/java/com/terra/develop/terra/Menu.java
+++ app/src/main/java/com/terra/develop/terra/Menu.java
@@ -1,10 +1,11 @@
 package com.terra.develop.terra;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
